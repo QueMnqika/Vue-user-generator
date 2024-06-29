24f8dee5a0494ab33912bde08db4379f05dd300f
Que Mnqika <114352483+QueMnqika@users.noreply.github.com>
2024-06-29 17:15:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            picture: 'https://randomuser.me/portraits/men/1.jpg',
+            firstName: 'Michael',
+            lastName: 'Johnson',
+            email: 'michael.johnson@example.com',
+            gender: 'male',
+        }
+    },
+    methods: {
+        async getUsers(){
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            console.log(results)
+        
+            this.picture = results[0].picture.large
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+        },
+    }
+})
+
+
+app.mount('#app')
